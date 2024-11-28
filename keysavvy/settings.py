52b77373d5a97a5f53b26ece8117cd58508610f9
umairwebdeveloper <umairashraf5252@gmail.com>
2024-11-28 23:07:14 +0500

    fic

--- keysavvy/settings.py
+++ keysavvy/settings.py
@@ -18,8 +18,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
-
+try:
+    load_dotenv(BASE_DIR / '.env')
+except:
+    print("env not load")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
